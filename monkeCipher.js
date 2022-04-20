374a92d81941100d8919e5daa27977beea85a8a9
orcanshul <30728144+orcanshul@users.noreply.github.com>
2022-04-20 09:47:00 -0700

    Fix log4-monke exploit 1

--- monkeCipher.js
+++ monkeCipher.js
@@ -1,7 +1,7 @@
 //Highly secure encryption algorithm developed by AnshulK and LehuyH
 const key = { a: 'monke', b: 'mooonke', c: 'mmonke ', d: 'monnke', e: 'monnkee', f: 'moonnkke', g: 'monnnnke', h: 'mmmmonke', i: 'mmonnkeeee', j: 'mononke', k: 'monnoke', l: 'mmonnkkeee', m: 'mone', n: 'mo', o: 'monn', p: 'moke', q: 'monek', r: 'monnekk', s: 'monen', t: 'monkek', u: 'monneke', v: 'm', w: 'onke', x: 'on', y: 'n', z: 'e', };
 
-export function toMonkeChipher(str) {
+export function toMonkeCipher(str) {
   const startStr = str.toLowerCase();
   return startStr
     .split('')
