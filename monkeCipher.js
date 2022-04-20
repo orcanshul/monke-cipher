cefe177ef584518cfef386d4071b33fa770b563e
orcanshul <30728144+orcanshul@users.noreply.github.com>
2022-04-20 09:38:22 -0700

    Create monke-cipher

---
+++ monkeCipher.js
@@ -0,0 +1,10 @@
+//Highly secure encryption algorithm developed by AnshulK and LehuyH
+const key = { a: 'monke', b: 'mooonke', c: 'mmonke ', d: 'monnke', e: 'monnkee', f: 'moonnkke', g: 'monnnnke', h: 'mmmmonke', i: 'mmonnkeeee', j: 'mononke', k: 'monnoke', l: 'mmonnkkeee', m: 'mone', n: 'mo', o: 'monn', p: 'moke', q: 'monek', r: 'monnekk', s: 'monen', t: 'monkek', u: 'monneke', v: 'm', w: 'onke', x: 'on', y: 'n', z: 'e', };
+
+export function toMonkeChipher(str) {
+  const startStr = str.toLowerCase();
+  return startStr
+    .split('')
+    .map((e) => key[e] || ' ')
+    .join('');
+}
